1048ef91a938adfe1098f24538319d9b4a4119b3
cosimolacava-del <cosimo.lacava@gmail.com>
2026-09-23 10:46:23 +0200

    Connect automatic interest form to association's Apps Script endpoint

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Nessuna risposta viene inviata o salvata se l'endpoint non è configurato.
 window.BIKE_TO_SCHOOL = Object.freeze({
   delivery: "sheet",
-  endpoint: ""
+  endpoint: "https://script.google.com/macros/s/AKfycbyjtboCqSKuKKHjIFNkRtNejnr0QH7IkfY-3_sEpMY2MrD81lHMeci2DPMw0Es-gmj7/exec"
 });
